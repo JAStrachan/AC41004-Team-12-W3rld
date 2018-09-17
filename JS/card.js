aaa6aa2d4e805cb87dc37380e21812da7c1dff49
JAStrachan <jstrachansss@gmail.com>
2018-09-17 14:45:04 +0100

    Refactored into multiple functions

--- JS/card.js
+++ JS/card.js
@@ -18,98 +18,12 @@ class Card {
         valDiv.className = "value";
         valDiv.textContent = this.value;
 
-		 // create div to store the time
-        let TimeDiv = document.createElement("div");
-        TimeDiv.className = "time";
-        TimeDiv.textContent = "13:38";
-
-        // create div to store SVG icon
-        let iconDiv = document.createElement("div");
-        iconDiv.className = "flexcontainer icon";
-
-            // add svg into iconDiv
-            let svgIcon = document.createElement("object");
-            svgIcon.type = "image/svg+xml";
-            svgIcon.data = "SVG/thermometer.svg";
-            svgIcon.style.height = "calc(var(--card-height) - var(--button-height))";
-            svgIcon.style.width = "100%";
-
-            iconDiv.appendChild(svgIcon);
-
-        // create div to store slider
-        let sliderDiv = document.createElement("div");
-        sliderDiv.className = "slidercontainer";
-
-            // add slider to div
-            let slider = document.createElement("input");
-            slider.type = "range";
-            slider.id = "range";
-            slider.className = "slider";
-            slider.style.min = "1";
-            slider.style.max = "12";
-            slider.style.value = "12";
-
-            sliderDiv.appendChild(slider);
-
-        // create div to store graph
-        let graphDiv = document.createElement("div");
-        graphDiv.className = "graph";
-        let graphCanvas = document.createElement("canvas");
-        graphCanvas.id = "graphCanvas";
-        graphDiv.appendChild(graphCanvas);
+        let TimeDiv = this.getTimeDiv();
+        let sliderDiv = this.getSliderDiv();
+        let iconDiv = this.getIconDiv();
+        let buttonDiv = this.getButtonDiv();
+        let graphDiv = this.getGraphDiv();
         
-
-        // create div to store expand/contract button
-        let buttonDiv = document.createElement("div");
-        buttonDiv.className = "button";
-        let arrow = document.createElement("i");
-        arrow.className = "arrow";
-        buttonDiv.appendChild(arrow);
-        buttonDiv.addEventListener("click", function() {
-            if(!this.expanded) {
-                this.expanded = true;
-
-                // flip arrow
-                arrow.style.transform = "rotate(225deg)";
-                arrow.style.marginTop = "5px";
-                arrow.style.marginBottom = "auto";
-
-                // expand card
-                cardDiv.style.height = "calc(var(--card-height-expanded) + var(--graph-height))";
-                cardDiv.style.width = "var(--card-width-expanded)";
-
-                // get svgobject
-                var doc = svgIcon.contentDocument;
-                var percentages = doc.getElementsByClassName("level");
-                var decimals = doc.getElementsByClassName("levelAnim");
-
-                // set new percentages
-                for(var i=0; i<percentages.length; i++) {
-                    percentages[i].setAttribute("offset", "60%");
-                }
-                // set new animation points
-                for(var i=0; i<decimals.length; i++) {
-                    decimals[i].setAttribute("to", "0.6");
-                }
-
-                // loading graph for sensor
-                var graph = new Graph();
-                graph.createGraph();
-            }
-            else {
-                this.expanded = false;
-
-                // flip arrow
-                arrow.style.transform = "rotate(45deg)";
-                arrow.style.marginTop = "auto";
-                arrow.style.marginBottom = "5px";
-
-                // // shrink card
-                cardDiv.style.height = "";
-                cardDiv.style.width = "";
-            }
-        });
-
         // combine elements into card
 		container.appendChild(valDiv);
 		container.appendChild(TimeDiv);
@@ -119,5 +33,121 @@ class Card {
         cardDiv.appendChild(buttonDiv);
         cardDiv.appendChild(graphDiv);
         return cardDiv;
+    }
+
+    // create div to store the time
+    getTimeDiv()
+    {
+        let TimeDiv = document.createElement("div");
+        TimeDiv.className = "time";
+        TimeDiv.textContent = "13:38";
+        return TimeDiv;
+    }
+
+    getIconDiv()
+    {
+        // create div to store SVG icon
+        let iconDiv = document.createElement("div");
+        iconDiv.className = "flexcontainer icon";
+
+        // add svg into iconDiv
+        let svgIcon = document.createElement("object");
+        svgIcon.type = "image/svg+xml";
+        svgIcon.data = "SVG/thermometer.svg";
+        svgIcon.style.height = "calc(var(--card-height) - var(--button-height))";
+        svgIcon.style.width = "100%";
+
+        iconDiv.appendChild(svgIcon);
+        return iconDiv;
+    }
+
+    getSliderDiv()
+    {
+        // create div to store slider
+        let sliderDiv = document.createElement("div");
+        sliderDiv.className = "slidercontainer";
+
+        // add slider to div
+        let slider = document.createElement("input");
+        slider.type = "range";
+        slider.id = "range";
+        slider.className = "slider";
+        slider.style.min = "1";
+        slider.style.max = "12";
+        slider.style.value = "12";
+
+        sliderDiv.appendChild(slider);
+        return sliderDiv;
+    }
+
+    getGraphDiv()
+    {
+        // create div to store graph
+        let graphDiv = document.createElement("div");
+        graphDiv.className = "graph";
+        let graphCanvas = document.createElement("canvas");
+        graphCanvas.id = "graphCanvas";
+        graphDiv.appendChild(graphCanvas);
+        return graphDiv;
+    }
+
+    getButtonDiv()
+    {
+        // create div to store expand/contract button
+        let buttonDiv = document.createElement("div");
+        buttonDiv.className = "button";
+
+        let arrow = document.createElement("i");
+        arrow.className = "arrow";
+        buttonDiv.appendChild(arrow);
+
+        buttonDiv.addEventListener("click", expandCard());
+    }
+
+    expandCard()
+    {
+        if(!this.expanded) {
+            this.expanded = true;
+
+            // flip arrow
+            arrow.style.transform = "rotate(225deg)";
+            arrow.style.marginTop = "5px";
+            arrow.style.marginBottom = "auto";
+
+            // expand card
+            cardDiv.style.height = "calc(var(--card-height-expanded) + var(--graph-height))";
+            cardDiv.style.width = "var(--card-width-expanded)";
+
+            // get svgobject
+            var doc = svgIcon.contentDocument;
+            var percentages = doc.getElementsByClassName("level");
+            var decimals = doc.getElementsByClassName("levelAnim");
+
+            // set new percentages
+            for(var i=0; i<percentages.length; i++) {
+                percentages[i].setAttribute("offset", "60%");
+            }
+
+            // set new animation points
+            for(var i=0; i<decimals.length; i++) {
+                decimals[i].setAttribute("to", "0.6");
+            }
+
+            // loading graph for sensor
+            var graph = new Graph();
+            graph.createGraph();
+        }
+        else {
+            this.expanded = false;
+
+            // flip arrow
+            arrow.style.transform = "rotate(45deg)";
+            arrow.style.marginTop = "auto";
+            arrow.style.marginBottom = "5px";
+
+            // // shrink card
+            cardDiv.style.height = "";
+            cardDiv.style.width = "";
+        } 
     }
 }
