d6f5428a7d01ac5a4c8ac9a041a6413869daa973
LiamRattray <ltrattray@dundee.ac.uk>
2018-09-18 16:12:00 +0100

    Added a tail to the markers

--- JS/marker.js
+++ JS/marker.js
@@ -13,11 +13,18 @@ function getDataMarkerIcon(sensorReading, svgPath) {
     readingText.innerHTML = sensorReading;
     readingText.className = "markerText";
 
+    let tail = document.createElement("div");
+    tail.className = "anchorTail";
+    let dot = document.createElement("div");
+    dot.className = "anchorDot"
+    tail.appendChild(dot);
+
     svgDiv.appendChild(svgObject);
     readingDiv.appendChild(readingText);
     let container = document.createElement("div");
     container.appendChild(svgDiv);
     container.appendChild(readingDiv);
+    container.appendChild(tail);
 
-    return L.divIcon({className: "divIcon", html: container.innerHTML, iconSize: [50, 50], iconAnchor: [25, 60]});
+    return L.divIcon({className: "divIcon has-anchor", html: container.innerHTML, iconSize: [50, 50], iconAnchor: [25, 75]});
 }
