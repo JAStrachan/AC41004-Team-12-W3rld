551621e2ba2edd9742b0e30db0c805927723a02d
LiamRattray <ltrattray@dundee.ac.uk>
2018-09-27 14:38:20 +0100

    Added conversion method to temperature object to convert entire dataset

--- JS/card.js
+++ JS/card.js
@@ -60,7 +60,7 @@ function getDayOfSensorData(sensorData)
 class Card {
 
     constructor(measurement, sensorData, tempFormat) {
-        this.lastReading = convert(tempFormat, sensorData[sensorData.length - 1].reading);
+        this.lastReading = sensorData[sensorData.length - 1].reading;
         this.lastTime = sensorData[sensorData.length - 1].time;
         this.lastDate = sensorData[sensorData.length - 1].date;
         this.units = measurement.units;
--- JS/loadingData.js
+++ JS/loadingData.js
@@ -5,20 +5,6 @@ class Reading{
         this.reading = reading;
     }
 }
-//Takes in input of mode desired to be displayed and converts values to appropriate unit when being displayed
-function convert(mode,value){
-    switch(mode){
-        //Kelvin
-        case "K":
-            return Math.round(value*100)/100;
-        //Fahrenheit
-        case "F":
-            return Math.round((value*1.8-459.67)*100) / 100;
-        //Celsius
-        case "C":
-            return Math.round((value-273.15) * 100) / 100;
-    }
-}
 
 function sensorAverage(mode) {
     //Displays the average temperature of the 4 sensors
@@ -74,7 +60,7 @@ getSettings = function(){
 
             settingsReadings.push(settings[0].TemperatureFormat);
             settingsReadings.push(settings[0].TimeInterval)
-            
+
             resolve(settingsReadings);
         }
     });
